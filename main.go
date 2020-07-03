6b7bb71e55e96cb932c71c9ed2f649a677911d58
Vinay Kumar Y N <VinayKumarYN@users.noreply.github.com>
2020-07-03 15:31:30 +0530

    Update main.go

--- main.go
+++ main.go
@@ -3,115 +3,111 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"regexp"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
-//Response struct
 type Response struct {
-	Success bool        `json:"success"`
-	Data    interface{} `json:"data"`
-	Error   string      `json:"error"`
+	Success bool        `json:"Success"`
+	Data    interface{} `json:"Data"`
+	Error   string      `json:"Error"`
+}
+type IPAddress struct {
+	Data struct {
+		Attributes struct {
+			AsOwner             string `json:"as_owner"`
+			Asn                 int64  `json:"asn"`
+			Continent           string `json:"continent"`
+			Country             string `json:"country"`
+			LastAnalysisResults interface {
+			} `json:"last_analysis_results"`
+			LastAnalysisStats struct {
+				Harmless   int64 `json:"harmless"`
+				Malicious  int64 `json:"malicious"`
+				Suspicious int64 `json:"suspicious"`
+				Timeout    int64 `json:"timeout"`
+				Undetected int64 `json:"undetected"`
+			} `json:"last_analysis_stats"`
+			LastModificationDate     int64         `json:"last_modification_date"`
+			Network                  string        `json:"network"`
+			RegionalInternetRegistry string        `json:"regional_internet_registry"`
+			Reputation               int64         `json:"reputation"`
+			Tags                     []interface{} `json:"tags"`
+			TotalVotes               struct {
+				Harmless  int64 `json:"harmless"`
+				Malicious int64 `json:"malicious"`
+			} `json:"total_votes"`
+			Whois     string `json:"whois"`
+			WhoisDate int64  `json:"whois_date"`
+		} `json:"attributes"`
+		ID    string `json:"id"`
+		Links struct {
+			Self string `json:"self"`
+		} `json:"links"`
+		Type string `json:"type"`
+	} `json:"data"`
 }
 
-//Post structure
-type ipInfo struct {
-	City          string  `json:"city"`
-	ContinentCode string  `json:"continent_code"`
-	ContinentName string  `json:"continent_name"`
-	CountryCode   string  `json:"country_code"`
-	CountryName   string  `json:"country_name"`
-	IP            string  `json:"ip"`
-	Latitude      float64 `json:"latitude"`
-	Location      struct {
-		CallingCode             string `json:"calling_code"`
-		Capital                 string `json:"capital"`
-		CountryFlag             string `json:"country_flag"`
-		CountryFlagEmoji        string `json:"country_flag_emoji"`
-		CountryFlagEmojiUnicode string `json:"country_flag_emoji_unicode"`
-		GeonameID               int64  `json:"geoname_id"`
-		IsEu                    bool   `json:"is_eu"`
-		Languages               []struct {
-			Code   string `json:"code"`
-			Name   string `json:"name"`
-			Native string `json:"native"`
-		} `json:"languages"`
-	} `json:"location"`
-	Longitude  float64 `json:"longitude"`
-	RegionCode string  `json:"region_code"`
-	RegionName string  `json:"region_name"`
-	Type       int32   `json:"type"`
-	Zip        string  `json:"zip"`
+func validIP4(ipAd string) bool {
+	ipAd = strings.Trim(ipAd, " ")
+	re, _ := regexp.Compile(`^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`)
+	if re.MatchString(ipAd) {
+		return true
+	}
+	return false
 }
 
-//var posts []Post
-// func getPosts(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	json.NewEncoder(w).Encode(posts)
-// }
-// func createPost(w http.ResponseWriter, r *http.Request) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	var post Post
-// 	_ = json.NewDecoder(r.Body).Decode(&post)
-
-// 	posts = append(posts, post)
-// 	json.NewEncoder(w).Encode(post)
-// }
-func getIPInfo(w http.ResponseWriter, r *http.Request) {
+func getIPAdress(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
+	var respo Response
 	ip := params["ip"]
-	var ipStruct ipInfo
-	var resp Response
-
-	url := "http://api.ipstack.com/" + ip + "?access_key=4dbe5149a238b4d52d8d7788c7e7c39f"
-	//method := "GET"
-
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		resp.Success = false
-		resp.Error = err.Error()
-		json.NewEncoder(w).Encode(resp)
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		resp.Success = false
-		resp.Error = err.Error()
-		json.NewEncoder(w).Encode(resp)
-	}
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		resp.Success = false
-		resp.Error = err.Error()
-		json.NewEncoder(w).Encode(resp)
-
-	}
-	//for displaying in postman
-	err = json.Unmarshal(body, &ipStruct)
-	//fmt.Println(string(body))
-	if err != nil {
-		resp.Success = false
-		resp.Error = err.Error()
-		json.NewEncoder(w).Encode(resp)
+	err := validIP4(ip)
+	if err == false {
+		respo.Success = false
+		respo.Error = "IP Should not match"
+		json.NewEncoder(w).Encode(respo)
 	} else {
-		resp.Success = true
-		resp.Data = ipStruct
-		json.NewEncoder(w).Encode(resp)
-
+		var ipadd IPAddress
+		//var respo Response
+		client := &http.Client{}
+		req, err := http.NewRequest("GET", "https://www.virustotal.com/api/v3/ip_addresses/"+ip, nil)
+		if err != nil {
+			respo.Success = false
+			respo.Error = err.Error()
+			json.NewEncoder(w).Encode(respo)
+		}
+		req.Header.Add("x-apikey", "96bb3b282eaa92a99dc48c0bf0e9f817077845c10b29f3f1534535bca767f03a")
+		res, err := client.Do(req)
+		if err != nil {
+			respo.Success = false
+			respo.Error = err.Error()
+			json.NewEncoder(w).Encode(respo)
+		}
+		defer r.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			respo.Success = false
+			respo.Error = err.Error()
+			json.NewEncoder(w).Encode(respo)
+		}
+		err = json.Unmarshal(body, &ipadd)
+		if err != nil {
+			respo.Success = false
+			respo.Error = err.Error()
+			json.NewEncoder(w).Encode(respo)
+		} else {
+			json.NewEncoder(w).Encode(ipadd)
+		}
 	}
 
-	//}
 }
-
 func main() {
-	router := mux.NewRouter()
-	//posts = append(posts, Post{ID: "1", Title: "My first post", Body: "This is the content of my first post"})
-	//router.HandleFunc("/getdata", getPosts).Methods("GET")
-	//router.HandleFunc("/postdata", createPost).Methods("POST")
-	router.HandleFunc("/getipinfo/{ip}", getIPInfo).Methods("GET")
-
-	http.ListenAndServe(":8000", router)
+	r := mux.NewRouter()
+	r.HandleFunc("/api/IPaddress/{ip}", getIPAdress).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
